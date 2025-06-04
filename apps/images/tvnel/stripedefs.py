fcbb1261fd78e20e19ea9c836b63bfd1c0b3a9ac
DeadGit <adeadgit@gmail.com>
2025-06-04 17:02:25 -0300

    add last minute high score

--- apps/images/tvnel/stripedefs.py
+++ apps/images/tvnel/stripedefs.py
@@ -6,9 +6,11 @@ stripes = [
         strip("bola.png", frames=12),
         strip("moregrass.png", frames=4),
         strip("bricks.png"),
+        strip("numerals.png", frames=12),
     ),
     palettegroup(
         fullscreen("fondo2.png"),
         fullscreen("bottom.png"),
     ),
 ]
+
